be18172ea77c827445f3eb6b0e5de5d41df5d919
Sundeep Chand <sundeep.chand2001@gmail.com>
2020-11-21 19:26:35 +0530

    Almost done

--- background.js
+++ background.js
@@ -1,148 +1,136 @@
 let defaultTime = {
-    hours: 0,
-    minutes: 30,
-    seconds: 0,
+  hours: 0,
+  minutes: 30,
+  seconds: 0,
 };
 let timer = null;
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ time: defaultTime }, () => {
-        timer = new Timer();
-        chrome.storage.sync.get(['time'], ({ time }) => {
-            defaultTime = {
-                hours: time.hours,
-                minutes: time.minutes,
-                seconds: time.seconds,
-            };
-        })
-    });
+  chrome.storage.sync.set({ time: defaultTime }, () => {
+    timer = new Timer();
+    chrome.storage.sync.get(['time'], ({ time }) => {
+      defaultTime = {
+        hours: time.hours,
+        minutes: time.minutes,
+        seconds: time.seconds,
+      };
+    })
+  });
 });
 
 let handle = 0;
 class Timer {
-    constructor() {
-        this.currentTime = {
-            hours: defaultTime.hours,
-            minutes: defaultTime.minutes,
-            seconds: defaultTime.seconds,
-        };
-        chrome.storage.sync.get(['time'], ({ time }) => {
-            this.currentTime = {
-                hours: time.hours,
-                minutes: time.minutes,
-                seconds: time.seconds,
-            };
-        });
-        this.currentState = {
-            paused: false,
-            running: false,
-            finished: false,
-        };
-    }
-    init(hh, mm, ss) {
-        this.currentTime = {
-            hours: hh,
-            minutes: mm,
-            seconds: ss,
-        };
-        this.currentState = {
-            paused: false,
-            running: false,
-            finished: false,
-        };
-    }
-    resetState = () => {
-        this.currentState.paused = false;
-        this.currentState.running = false;
-        this.currentState.finished = false;
-    }
-    reset = () => {
-        chrome.storage.sync.get(['time'], ({ time }) => {
-            this.currentTime = {
-                hours: time.hours,
-                minutes: time.minutes,
-                seconds: time.seconds,
-            };
-        });
-        this.resetState();
-    }
-    pause = () => {
-        clearInterval(handle);
-        handle = 0;
-        this.currentState.paused = true;
-        this.currentState.running = false;
-    }
-    setTime = (time) => {
-        this.currentTime.hours = time.hours;
-        this.currentTime.minutes = time.minutes;
-        this.currentTime.seconds = time.seconds;
-    }
-    decrement = () => {
-        if (this.currentTime.hours >= 0 && this.currentTime.minutes >= 0 && this.currentTime.seconds >= 0) {
-            this.currentTime.seconds--;
-            if (this.currentTime.seconds < 0) {
-                this.currentTime.seconds = 59;
-                this.currentTime.minutes--;
-            }
-            if (this.currentTime.minutes < 0) {
-                this.currentTime.minutes = 59;
-                this.currentTime.hours--;
-            }
-        } else {
-            // Timer is over.
-            this.reset();
-            clearInterval(handle);
-            handle = 0;
-            chrome.tts.speak(`Time's up!`, {
-                'lang': 'en-US',
-                'rate': 1.5,
-                'pitch': 1.5,
-            });
-        }
+  constructor() {
+    this.currentTime = {
+      hours: defaultTime.hours,
+      minutes: defaultTime.minutes,
+      seconds: defaultTime.seconds,
     };
+    chrome.storage.sync.get(['time'], ({ time }) => {
+      this.currentTime = {
+        hours: time.hours,
+        minutes: time.minutes,
+        seconds: time.seconds,
+      };
+    });
+    this.currentState = {
+      paused: false,
+      running: false,
+      finished: false,
+    };
+  }
+  resetState = () => {
+    this.currentState.paused = false;
+    this.currentState.running = false;
+    this.currentState.finished = false;
+  }
+  reset = () => {
+    chrome.storage.sync.get(['time'], ({ time }) => {
+      this.currentTime = {
+        hours: time.hours,
+        minutes: time.minutes,
+        seconds: time.seconds,
+      };
+    });
+    this.resetState();
+  }
+  pause = () => {
+    clearInterval(handle);
+    handle = 0;
+    this.currentState.paused = true;
+    this.currentState.running = false;
+  }
+  setTime = (time) => {
+    this.currentTime.hours = time.hours;
+    this.currentTime.minutes = time.minutes;
+    this.currentTime.seconds = time.seconds;
+  }
+  decrement = () => {
+    if (this.currentTime.hours >= 0 && this.currentTime.minutes >= 0 && this.currentTime.seconds >= 0) {
+      this.currentTime.seconds--;
+      if (this.currentTime.seconds < 0) {
+        this.currentTime.seconds = 59;
+        this.currentTime.minutes--;
+      }
+      if (this.currentTime.minutes < 0) {
+        this.currentTime.minutes = 59;
+        this.currentTime.hours--;
+      }
+    } else {
+      // Timer is over.
+      this.reset();
+      clearInterval(handle);
+      handle = 0;
+      chrome.tts.speak(`Time's up!`, {
+        'lang': 'en-US',
+        'rate': 1.5,
+        'pitch': 1.5,
+      });
+    }
+  };
 };
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.cmd === 'START_TIMER' && !timer.currentState.running)
-    {
-        if (!timer.currentState.paused) {
-            timer.reset();
-        }
-        timer.currentState.running = true;
-        handle = setInterval(timer.decrement, 1000);
-        sendResponse({ time: timer.currentTime, running: timer.currentState.running });
+  if (request.cmd === 'START_TIMER' && !timer.currentState.running)
+  {
+    if (!timer.currentState.paused) {
+      timer.reset();
     }
-    else if (request.cmd === 'SET_TIME' && !timer.currentState.running)
-    {
-        chrome.storage.sync.set({ time: request.time });
-        defaultTime = {
-            hours: request.time.hours,
-            minutes: request.time.minutes,
-            seconds: request.time.seconds,
-        };
-        timer = new Timer();
-    }
-    else if (request.cmd === 'STOP_TIMER' && timer.currentState.running)
-    {
-        timer.pause();
-        clearInterval(handle);
-        handle = 0;
-        sendResponse({ time: timer.currentTime, running: timer.currentState.running });
-    }
-    else if (request.cmd === 'RESET_TIMER' && !timer.currentState.running)
-    {
-        timer = new Timer();
-        sendResponse({ time: timer.currentTime, running: timer.currentState.running });
-    }
-    else if (request.cmd === 'GET_TIME')
-    {
-        sendResponse({
-            time: timer.currentTime,
-            running: timer.currentState.running,
-            paused: timer.currentState.paused
-        });
-    }
-    else
-    {
-        sendResponse(null);
-    }
+    timer.currentState.running = true;
+    handle = setInterval(timer.decrement, 1000);
+    sendResponse({ time: timer.currentTime, running: timer.currentState.running });
+  }
+  else if (request.cmd === 'SET_TIME' && !timer.currentState.running)
+  {
+    chrome.storage.sync.set({ time: request.time });
+    defaultTime = {
+      hours: request.time.hours,
+      minutes: request.time.minutes,
+      seconds: request.time.seconds,
+    };
+    timer = new Timer();
+  }
+  else if (request.cmd === 'STOP_TIMER' && timer.currentState.running)
+  {
+    timer.pause();
+    clearInterval(handle);
+    handle = 0;
+    sendResponse({ time: timer.currentTime, running: timer.currentState.running });
+  }
+  else if (request.cmd === 'RESET_TIMER' && !timer.currentState.running)
+  {
+    timer = new Timer();
+    sendResponse({ time: timer.currentTime, running: timer.currentState.running });
+  }
+  else if (request.cmd === 'GET_TIME')
+  {
+    sendResponse({
+      time: timer.currentTime,
+      running: timer.currentState.running,
+      paused: timer.currentState.paused
+    });
+  }
+  else
+  {
+    sendResponse(null);
+  }
 });
--- popup.js
+++ popup.js
@@ -7,121 +7,120 @@ const btnSetTime = document.getElementById('btn-set-time');
 const btnStop = document.getElementById('btn-stop');
 const btnReset = document.getElementById('btn-reset');
 
-let handle = 0;
-chrome.runtime.sendMessage({ cmd: 'GET_TIME' }, (response) => {
-    if (response) {
-        if (response.time) {
-            showTime(response.time);
-            if (response.running) {
-                handle = setInterval(() => {
-                    decrement(response.time);
-                    showTime(response.time);
-                }, 1000);
-            }
-        }
-        if (response.running) {
-            disableButtons(true, true, false, true);
-        } else if (response.paused) {
-            disableButtons(false, true, true, false);
-        }
+const afterGetMessage = (response) => {
+  if (response) {
+    if (response.time) {
+      showTime(response.time);
+      if (response.running) {
+        handle = setInterval(() => {
+          decrement(response.time);
+          showTime(response.time);
+        }, 1000);
+      }
     }
-})
+    if (response.running) {
+      disableButtons(true, true, false, true);
+    } else if (response.paused) {
+      disableButtons(false, true, true, false);
+    }
+  }
+}
+
+let handle = 0;
+chrome.runtime.sendMessage({ cmd: 'GET_TIME' }, afterGetMessage);
 
 const toggleInputFieldDisabled = () => {
-    hours.disabled = !hours.disabled;
-    minutes.disabled = !minutes.disabled;
-    seconds.disabled = !seconds.disabled;
+  hours.disabled = !hours.disabled;
+  minutes.disabled = !minutes.disabled;
+  seconds.disabled = !seconds.disabled;
 }
 const disableButtons = (start = false, setTime = false, Stop = false, reset = false) => {
-    btnReset.disabled = reset;
-    btnStart.disabled = start;
-    btnStop.disabled = Stop;
-    btnSetTime.disabled = setTime;
+  btnReset.disabled = reset;
+  btnStart.disabled = start;
+  btnStop.disabled = Stop;
+  btnSetTime.disabled = setTime;
 }
 
 const showTime = (time) => {
-    if (time.hours < 0) {
-        time.hours = time.minutes = time.seconds = 0;
-    }
-    hours.value = time.hours < 10 ? `0${time.hours}` : time.hours;
-    minutes.value = time.minutes < 10 ? `0${time.minutes}` : time.minutes;
-    seconds.value = time.seconds < 10 ? `0${time.seconds}` : time.seconds;
+  if (time.hours < 0) {
+    time.hours = time.minutes = time.seconds = 0;
+  }
+  hours.value = time.hours < 10 ? `0${time.hours}` : time.hours;
+  minutes.value = time.minutes < 10 ? `0${time.minutes}` : time.minutes;
+  seconds.value = time.seconds < 10 ? `0${time.seconds}` : time.seconds;
 }
 
 const decrement = (time) => {
-    if (time.hours >= 0 && time.minutes >= 0 && time.seconds >= 0) {
-        time.seconds--;
-        if (time.seconds < 0) {
-            time.seconds = 59;
-            time.minutes--;
-        }
-        if (time.minutes < 0) {
-            time.minutes = 59;
-            time.hours--;
-        }
-    } else {
-        // Timer is over.
-        clearInterval(handle);
-        handle = 0;
+  if (time.hours >= 0 && time.minutes >= 0 && time.seconds >= 0) {
+    time.seconds--;
+    if (time.seconds < 0) {
+      time.seconds = 59;
+      time.minutes--;
     }
+    if (time.minutes < 0) {
+      time.minutes = 59;
+      time.hours--;
+    }
+  } else {
+    // Timer is over.
+    clearInterval(handle);
+    handle = 0;
+  }
 };
 
 btnStart.onclick = () => {
-    chrome.runtime.sendMessage({ cmd: 'START_TIMER' }, (response) => {
-        if (response && response.time) {
-            disableButtons(true, true, false, true);
-            showTime(response.time);
-            if (response.running) {
-                handle = setInterval(() => {
-                    decrement(response.time);
-                    showTime(response.time);
-                }, 1000);
-            }
-        }
-    });
+  chrome.runtime.sendMessage({ cmd: 'START_TIMER' }, (response) => {
+    if (response && response.time) {
+      disableButtons(true, true, false, true);
+      showTime(response.time);
+      if (response.running) {
+        handle = setInterval(() => {
+          decrement(response.time);
+          showTime(response.time);
+        }, 1000);
+      }
+    }
+  });
 }
 
 btnSetTime.onclick = () => {
-    console.log(btnSetTime);
-    if (btnSetTime.textContent === 'SET TIME')
-    {
-        disableButtons(true, false, true, true);
-        btnSetTime.textContent = 'SET';
-        toggleInputFieldDisabled();
-    }
-    else if (btnSetTime.textContent === 'SET')
-    {
-        btnSetTime.textContent = 'SET TIME';
-        disableButtons();
-        toggleInputFieldDisabled();
-        chrome.runtime.sendMessage({ cmd: 'SET_TIME', time: {
-            hours: Number(hours.value),
-            minutes: Number(minutes.value),
-            seconds: Number(seconds.value),
-        } });
-    }
+  if (btnSetTime.textContent === 'Set Time')
+  {
+    disableButtons(true, false, true, true);
+    btnSetTime.textContent = 'Done';
+    toggleInputFieldDisabled();
+  }
+  else if (btnSetTime.textContent === 'Done')
+  {
+    btnSetTime.textContent = 'Set Time';
+    disableButtons();
+    toggleInputFieldDisabled();
+    chrome.runtime.sendMessage({ cmd: 'SET_TIME', time: {
+      hours: Number(hours.value),
+      minutes: Number(minutes.value),
+      seconds: Number(seconds.value),
+    } });
+  }
 }
 
 btnStop.onclick = () => {
-    chrome.runtime.sendMessage({ cmd: 'STOP_TIMER' }, (response) => {
-        if (response && response.time) {
-            disableButtons(false, true, true, false);
-            showTime(response.time);
-            clearInterval(handle);
-            handle = 0;
-        }
-    });
+  chrome.runtime.sendMessage({ cmd: 'STOP_TIMER' }, (response) => {
+    if (response && response.time) {
+      disableButtons(false, true, true, false);
+      showTime(response.time);
+      clearInterval(handle);
+      handle = 0;
+    }
+  });
 }
 
 btnReset.onclick = () => {
-    chrome.runtime.sendMessage({ cmd: 'RESET_TIMER' }, (response) => {
-        console.log(response);
-        if (response && response.time) {
-            disableButtons();
-            showTime(response.time);
-            clearInterval(handle);
-            handle = 0;
-            console.log('Timer reset');
-        }
-    });
+  chrome.runtime.sendMessage({ cmd: 'RESET_TIMER' }, (response) => {
+    if (response && response.time) {
+      disableButtons();
+      showTime(response.time);
+      clearInterval(handle);
+      handle = 0;
+    }
+  });
 }
